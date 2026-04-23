02e584b552fcdc293fbe95114fbe4544f8f36158
iolq <wipawee.watt@gmail.com>
2026-04-23 11:40:58 +0700

    done

---
+++ 1_sizeof.cpp
@@ -0,0 +1,20 @@
+/*
+1. ให้ลองแสดงค่าขนาดของแต่ละตัวแปรต่อไปนี้แล้วสังเกตว่าออกมาเป็นเลขอะไรข้าง
+
+#input
+-
+
+#output
+string: 24 Bytes
+char: 1 Byte
+double: 8 Bytes
+float: 4 Bytes
+int: 4 Bytes
+
+*/
+
+string name = "Atom";
+char cha = 'A';
+double a = 1.33;
+float b = 1.33;
+int c = 0;
